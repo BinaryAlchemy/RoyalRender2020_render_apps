e81a8ee460c449e64a4919126cb2d4aedb487815
Holger-Elliot <mail@BinaryAlchemy>
2023-02-16 20:21:55 +0100

    Houdini 12 preparation

---
+++ _submitplugins/Houdini/python3.10libs/htorr/rrnode/rop/rr_vray.py
@@ -0,0 +1,219 @@
+# Last change: %rrVersion%
+# Copyright (c) Holger Schoenberger - Binary Alchemy
+
+import logging
+import os.path
+from htorr.rroutput import Output
+from htorr.rrnode.base import RenderNode
+
+
+logger = logging.getLogger("HtoRR")
+
+try:
+    import hou
+except ImportError:
+    logger.info("Module imported outside of hython environment")
+
+
+logger = logging.getLogger("HtoRR")
+
+try:
+    import hou
+except ImportError:
+    logger.info("Module imported outside of hython environment")
+
+
+
+class VrayRop(RenderNode):
+
+    name = "vray_renderer"
+
+    @property
+    def camera_parm(self):
+        return "render_camera"
+
+    @property
+    def output_parm(self):
+        return "SettingsOutput_img_file_path"
+                
+    @property
+    def renderer(self):
+        return "vray"
+
+    @property
+    def renderer_version(self):
+        try:
+            import vray 
+        except ImportError:
+            return ""
+
+        versionStr = vray.getVRayVersionDetails()
+        vray_ver = versionStr.split()[0]   
+        return vray_ver
+
+    @property
+    def image_size(self):
+        from htorr.rrnode.rop import utils
+        x = None
+        y = None
+        if not hou.node(self.camera):
+            return
+        try:
+            if not self._node.evalParm("override_camerares"):
+                x, y = utils.get_camera_res(self.camera)
+            else:
+                if self._node.evalParm("res_fraction") == "specific":
+                    x = self._node.evalParm("res_overridex")
+                    y = self._node.evalParm("res_overridey")
+                else:
+                    frac = float(self._node.evalParm("res_fraction"))
+                    x, y = utils.get_camera_res(self.camera)
+                    x = int(round(x * frac))
+                    y = int(round(y * frac))
+
+        except ValueError:
+            return
+
+        return(x, y)
+
+    @property
+    def aovs(self):
+        
+        # Check if variable AOV is used, if not, no AOVs will be written
+        raw_path = self._node.parm(self.output_parm).rawValue()
+        if not "$AOV" in raw_path and not "${AOV}" in raw_path:
+            return
+
+        
+        network = self._node.parm("render_network_render_channels").evalAsNode()
+        if not network:
+            return
+
+        aovs = []
+        channel_container = None
+        channels = []
+
+        # Get Channel Container node
+        for node in network.children():
+            if node.type().name() == "VRayNodeRenderChannelsContainer":
+                channel_container = node
+                break
+        
+        # Get all Render Element Nodes and copy names into channels list
+        if channel_container:
+            for input in channel_container.inputs():
+                if not input.isBypassed() and input.type().name() == "VRayNodeRenderChannelColor":
+                    channels.append(input.parm("name").eval())
+
+        for channel in channels:
+            try:
+                hou.putenv("AOV", channel)
+                out = Output(self._node.parm(self.output_parm))
+                aovs.append((os.path.join(out.dir,out.name),out.extension))
+            finally:
+                hou.unsetenv("AOV")
+
+        return aovs
+
+    @property
+    def archive(self):
+        return self._node.parm("render_export_mode").eval() != '0'
+
+    @property
+    def gpu(self):
+        if self._node.parm("render_render_mode").eval() == '0':
+            return False
+        else:
+            return True
+
+
+    def check(self):
+        if self._node.parm('SettingsOutput_relements_separate_rgba').eval():
+            logger.info("'{}': Feature 'Store Beauty Pass Under a Separate Folder' not yet supported".format(self.path))
+        return True
+
+    def to_archive(self):
+        self.__class__ = VrayArchiveROP
+
+    def to_standalone(self):
+        f1 = self._node.parm("render_export_filepath").evalAtFrame(1)
+        f2 = self._node.parm("render_export_filepath").evalAtFrame(2)
+        if f1 == f2:
+            self.__class__ = VrayStandalone
+        else:
+            self.__class__ = VrayStandalone_multifile
+
+
+class VrayArchiveROP(VrayRop):
+
+    name = "vray_renderer_archive"
+
+    @property
+    def renderer(self):
+        return "createVrScene"
+
+    @property
+    def output_parm(self):
+        return "render_export_filepath"
+
+    @property
+    def aovs(self):
+        return
+
+    @property
+    def gpu(self):
+        return False
+    
+    @property
+    def single_output(self):
+        f1 = self._node.parm(self.output_parm).evalAtFrame(1)
+        f2 = self._node.parm(self.output_parm).evalAtFrame(2)
+        if f1 == f2:
+            return True
+        else:
+            return False
+
+class VrayStandalone(VrayRop):
+
+    name = "vray_renderer_standalone"
+
+    @property
+    def software(self):
+        return "VRay_StdA"
+
+    @property
+    def software_version(self):
+        VRay_Version= VrayRop.renderer_version.fget(self)
+        logger.debug("{}: VRay_Version {}".format(self._node.path(), VRay_Version ))        
+        return VRay_Version
+
+    @property
+    def renderer(self):
+        return "Houdini"
+
+    @property
+    def renderer_version(self):
+        return
+
+
+class VrayStandalone_multifile(VrayRop):
+
+    name = "vray_renderer_standalone_multifile"
+
+    @property
+    def software(self):
+        return "VRay_StdA"
+
+    @property
+    def software_version(self):
+        VRay_Version= VrayRop.renderer_version.fget(self)
+        logger.debug("{}: VRay_Version {}".format(self._node.path(), VRay_Version ))        
+        return VRay_Version
+
+    @property
+    def renderer(self):
+        return "multifile"
+
+    @property
+    def renderer_version(self):
+        return
---
+++ _submitplugins/Houdini/python3.10libs/htorr/rrsubmitter.py
@@ -0,0 +1,206 @@
+# Last change: %rrVersion%
+# Copyright (c) Holger Schoenberger - Binary Alchemy
+
+"""@package rrsubmitter
+This module contains classes and methods to convert python objects to submittable XML files and to submit these to the farm.
+"""
+
+import os
+import sys
+
+import subprocess
+import re
+import random
+import logging
+
+from xml.etree.ElementTree import Element, SubElement
+import xml.etree.ElementTree as et
+import xml.dom.minidom
+
+from htorr import utils
+import tempfile
+
+logger = logging.getLogger("HtoRR")
+
+
+def serialize_to_xml(submission):
+    """Converts a submssion instance to an xml text.
+    
+    Arguments:
+        submission Submission -- submission instance
+    
+    Returns:
+        String -- XML Text
+    """
+    serializer = XML_Serializer()
+    submission.serialize(serializer)
+    return serializer.to_str()
+
+
+class XML_Serializer(object):
+    """Class to serialize objects to a xml text.
+       An objects needs to implement the method serialize in order to beeing serializable.
+    """
+    def __init__(self):
+        self._element = None
+        self._end = False
+
+    def to_str(self):
+        if (sys.version_info.major == 3) and (sys.version_info.minor>=8):
+            text = et.tostring(self._element, encoding="utf-8", xml_declaration=True)
+        else:
+            text = et.tostring(self._element, encoding="utf-8")
+            
+        if True:
+            dom = xml.dom.minidom.parseString(text)
+            text = dom.toprettyxml(encoding='utf-8')
+        return text
+
+    def add(self, element, value):
+        sub = SubElement(self._element, element)
+        text = str(value)
+        if sys.version_info.major == 2:
+            text = text if type(text) is unicode else text.decode("utf8")
+        sub.text = text
+
+    def start(self, name, attrib=None):
+        self._element = Element(name)
+        if attrib:
+            if (sys.version_info.major == 2):
+                for key, value in attrib.iteritems():
+                    self._element.attrib[key] = value
+            else:
+                for key, value in attrib.items():
+                    self._element.attrib[key] = value
+
+    def append(self, obj):
+        xml = XML_Serializer()
+        obj.serialize(xml)
+        self._element.append(xml.get_element())
+
+    def get_element(self):
+        return self._element
+
+    def end(self):
+        self._end = True
+
+
+class RrCmdSubmitter(object):
+    """Royal Render Commandline Submitter Base Class."""
+
+    def __init__(self):
+        """Init Submitter Object.
+
+        Arguments:
+            tmp_dir string -- Path to Tmp Dir to save XML File.
+        """
+        self._tmp_dir = tempfile.gettempdir()
+
+    def write_submission_file(self, text):
+        """Write Submission XML File to disk.
+
+        Arguments:
+            text string -- XML serialized text
+
+        Returns:
+            string - path to written xml file
+
+        """
+        random.seed()
+        filename = "rrSubmitHoudini_{}.xml".format(random.randrange(1000, 10000, 1))
+        path = os.path.join(self._tmp_dir, filename)
+        file_ = open(path, 'wb')
+        file_.write(text)
+        file_.close()
+
+        return path
+
+    @staticmethod
+    def get_rr_rrStartLocal_path():
+        """Return path to Command Submitter"
+        """
+        rr_root = utils.get_rr_root()
+        if ((sys.platform.lower() == "win32") or
+                (sys.platform.lower() == "win64")):
+            rr_submitter = rr_root + "\\bin\\win64\\rrStartLocal.exe"
+        elif (sys.platform.lower() == "darwin"):
+            rr_submitter = rr_root + "/bin/mac64/rrStartLocal"
+        else:
+            rr_submitter = rr_root + "/bin/lx64/rrStartLocal"
+
+        return rr_submitter
+        
+
+class RrCmdGlobSubmitter(RrCmdSubmitter):
+    def __init__(self):
+        super(RrCmdGlobSubmitter, self).__init__()
+
+
+
+    def submit(self, submission):
+        """Submit provided Submission instance to farm.
+        
+        Arguments:
+            submission Submission -- submission
+        
+        Returns:
+            int[] -- list of jobs ids
+        """
+        logger.debug("Submit: {}".format(submission))
+        subm_text = serialize_to_xml(submission)
+        subm_file_path = self.write_submission_file(subm_text)
+        command = (self.get_rr_rrStartLocal_path()
+                   + " -sameTerminal rrSubmitterconsole  \"" + subm_file_path
+                   + "\"" )
+
+        # Execute RR Submitter with Popen Instance
+        # Additional Settings to hide Window
+        startupinfo = None
+        if os.name == 'nt':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        proc = subprocess.Popen(command, startupinfo=startupinfo, stdout=subprocess.PIPE)
+
+        out = proc.communicate()[0]
+        out= out.decode('ascii')
+
+        if proc.returncode != 0:
+            logger.warning("RR Commandline Submitter cant submit Job:\n"+out)
+            return None
+            
+        #logger.debug("Commandline Submitter output\n-------------------------------\n{}\n------------------------------".format(out))
+        try:
+            outSplitted = out.split("Job IDs send: ")
+            #logger.debug("outSplitted\n-------------------------------\n{}\n------------------------------".format(outSplitted))
+            id_string= outSplitted[-1]
+            ids = re.findall(r'(\d{12,})', id_string)
+            logger.debug("{} Jobs submitted:{}".format(len(ids), ids))
+            return ids
+        except:
+            logger.debug("Jobs submitted")
+            ids = []
+            ids.append("noID")
+            return ""
+       
+
+
+class RrGuiSubmitter(RrCmdSubmitter):
+    def __init__(self):
+        super(RrGuiSubmitter, self).__init__()
+
+
+
+    def submit(self, submission):
+        """Submit submission instance to farm.
+        Using subprocess to create another process for submission.
+        """
+        logger.debug("Submit: \n {}".format(submission))
+        subm_text = serialize_to_xml(submission)
+        subm_file_path = self.write_submission_file(subm_text)
+        command = (self.get_rr_rrStartLocal_path()
+                   + " rrSubmitter \"" + subm_file_path
+                   + "\"")
+        if (not os.path.exists(self.get_rr_rrStartLocal_path())):
+            logger.warning("RR executable does not exist:\n" + self.get_rr_rrStartLocal_path())
+        else:
+            subprocess.Popen(command)
