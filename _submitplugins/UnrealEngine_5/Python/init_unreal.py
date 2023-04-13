df663523c9f9bcb160cdbf89f3d5bd5aa1166eca
pKrime <palucam@gmail.com>
2023-04-13 11:11:41 +0200

    unreal engine: RoyalSubmit is the default remote executor if None is set

--- _submitplugins/UnrealEngine_5/Python/init_unreal.py
+++ _submitplugins/UnrealEngine_5/Python/init_unreal.py
@@ -1,2 +1,8 @@
 # Add Royal Render Submission executor to Unreal Engine
+import unreal
 import MoviePipelineRoyalSubmit
+
+
+projectSettings = unreal.get_default_object(unreal.MovieRenderPipelineProjectSettings)
+if not projectSettings.default_remote_executor:
+    projectSettings.default_remote_executor = MoviePipelineRoyalSubmit.MoviePipelineRoyalSubmit
